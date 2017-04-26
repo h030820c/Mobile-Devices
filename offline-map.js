314908a32001ccc1dfbd24b186e62c38d1792f63
h030820c <stevenhooton@Stevens-iMac.local>
2017-04-26 18:08:21 +0100

    changes 3

--- offline-map.js
+++ offline-map.js
@@ -1,11 +1,1 @@
 var map = document.getElementById("map").style.display = "none";
-var timetables = document.getElementById("timetables");
-var recent = document.getElementsByClassName('recent-times');
-
-    for (var i = 0; i <= Timetable.length - 1; i++) {
-        timetables.innerHTML += "<div class='bus-number' id='" + i + "'><a href='timeTableResults.html' class='busroute'> <p class='bus-route-num'>" + Timetable[i].Route + "</p> <div  class='bus-locations'><p>" + Timetable[i].From + "-" + Timetable[i].To + "</p><div class='recent-times'></div></div></a> </div>";
-
-        for (var q = 0; q < 5; q++) {
-            recent[i].innerHTML += "<p>" + Timetable[i].Times[q] + "</p>";
-        }
-    }
--- script.js
+++ script.js
@@ -339,3 +339,16 @@ if (BusRouteNum) {
     }
 
 }
+
+
+
+var timetables = document.getElementById("timetables");
+var recent = document.getElementsByClassName('recent-time');
+
+    for (var i = 0; i <= Timetable.length - 1; i++) {
+        timetables.innerHTML += "<div class='bus-number' id='" + i + "'><a href='timeTableResults.html' class='busroute'> <p class='bus-route-num'>" + Timetable[i].Route + "</p> <div  class='bus-locations'><p>" + Timetable[i].From + "-" + Timetable[i].To + "</p><div class='recent-time'></div></div></a> </div>";
+
+        for (var q = 0; q < 5; q++) {
+            recent[i].innerHTML += "<p>" + Timetable[i].Times[q] + "</p>";
+        }
+    }
