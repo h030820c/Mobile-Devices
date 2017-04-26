1ba21830e6bcd052ccc398d92cd55d5417fb95e0
h030820c <stevenhooton@Stevens-iMac.local>
2017-04-26 18:11:13 +0100

    changes 5

--- script.js
+++ script.js
@@ -345,8 +345,8 @@ if (BusRouteNum) {
 var timetables = document.getElementById("timetables");
 var recent = document.getElementsByClassName('recent-time');
 
-    for (var i = 0; i <= Timetable.length - 1; i++) {
-        timetables.innerHTML += "<div class='bus-number' id='" + i + "'><a href='timeTableResults.html' class='busroute'> <p class='bus-route-num'>" + Timetable[i].Route + "</p> <div  class='bus-locations'><p>" + Timetable[i].From + "-" + Timetable[i].To + "</p><div class='recent-time'></div></div></a> </div>";
+    for (var j = 0; j <= Timetable.length - 1; j++) {
+        timetables.innerHTML += "<div class='bus-number' id='" + j + "'><a href='timeTableResults.html' class='busroute'> <p class='bus-route-num'>" + Timetable[j].Route + "</p> <div  class='bus-locations'><p>" + Timetable[j].From + "-" + Timetable[j].To + "</p><div class='recent-time'></div></div></a> </div>";
 
         for (var q = 0; q < 5; q++) {
             recent[i].innerHTML += "<p>" + Timetable[i].Times[q] + "</p>";
