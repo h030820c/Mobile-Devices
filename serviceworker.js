67d109b4794e82c77a872360aa86cdf688a9effa
h030820c <stevenhooton@Stevens-iMac.local>
2017-05-02 18:19:53 +0100

    add font to service worker

--- serviceworker.js
+++ serviceworker.js
@@ -12,7 +12,7 @@ var CACHED_URLS = [
     BASE_PATH + 'TsandCs.html',
     BASE_PATH + 'bus-route.html',
     BASE_PATH + 'search-results.html',
-    
+
     // Images for favicons
     BASE_PATH + 'icons/android-icon-36x36.png',
     BASE_PATH + 'icons/android-icon-48x48.png',
@@ -38,75 +38,73 @@ var CACHED_URLS = [
     // Manifest
     BASE_PATH + 'manifest.json',
   // CSS and fonts
+    'https://fonts.googleapis.com/css?family=Roboto',
     BASE_PATH + 'style.css'
 ];
 
 var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAYtiJpXh3CHsaOAncAEh8AXp6C9BoO6wY&callback=initMap';
 
-self.addEventListener('install', function(event) {
-  // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  var requestURL = new URL(event.request.url);
-  // Handle requests for index.html
-  if (requestURL.pathname === BASE_PATH + 'index.html') {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match('index.html').then(function(cachedResponse) {
-          var fetchPromise = fetch('index.html').then(function(networkResponse) {
-            cache.put('first.html', networkResponse.clone());
-            return networkResponse;
-          });
-          return cachedResponse || fetchPromise;
-        });
-      })
-    );
- // Handle requests for Google Maps JavaScript API file
-  } else if (requestURL.href === googleMapsAPIJS) {
-    event.respondWith(
-      fetch(
-        googleMapsAPIJS+'&'+Date.now(),
-        { mode: 'no-cors', cache: 'no-store' }
-      ).catch(function() {
-        return caches.match('offline-map.js');
-      })
-    );
-  } else if (
-    CACHED_URLS.includes(requestURL.href) ||
-    CACHED_URLS.includes(requestURL.pathname)
-  ) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-        });
-      })
-    );
-  }
-});
-
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('install', function (event) {
+    // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(CACHED_URLS);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    var requestURL = new URL(event.request.url);
+    // Handle requests for index.html
+    if (requestURL.pathname === BASE_PATH + 'index.html') {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(function (cache) {
+                return cache.match('index.html').then(function (cachedResponse) {
+                    var fetchPromise = fetch('index.html').then(function (networkResponse) {
+                        cache.put('first.html', networkResponse.clone());
+                        return networkResponse;
+                    });
+                    return cachedResponse || fetchPromise;
+                });
+            })
+        );
+        // Handle requests for Google Maps JavaScript API file
+    } else if (requestURL.href === googleMapsAPIJS) {
+        event.respondWith(
+            fetch(
+                googleMapsAPIJS + '&' + Date.now(), {
+                    mode: 'no-cors',
+                    cache: 'no-store'
+                }
+            ).catch(function () {
+                return caches.match('offline-map.js');
+            })
+        );
+    } else if (
+        CACHED_URLS.includes(requestURL.href) ||
+        CACHED_URLS.includes(requestURL.pathname)
+    ) {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(function (cache) {
+                return cache.match(event.request).then(function (response) {
+                    return response || fetch(event.request);
+                });
+            })
+        );
+    }
 });
 
 
-
-
-
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
